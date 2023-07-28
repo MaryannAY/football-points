e2983a7a5a08c9e46404fe93b2dc10e3776e83f4
MaryannAY <maryann.ahmed2@gmail.com>
2023-07-28 07:08:10 -0700

    Football-points solved

--- challenge.js
+++ challenge.js
@@ -1,2 +1,6 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+function calculatePoints(wins,points3,draw,point1,lost,point0) {
+    console.log(wins * points3 + draw * point1 + lost * point0)
+}
+calculatePoints(5,3,2,1,3,0)
